28831c7e9c96d8fa3162ea221f4f17eb8dd13210
Hungermanw3t <mowsonben@gmail.com>
2024-05-24 13:59:49 +1000

    added some of the json components. need to add the fruit class

--- fruit/Program.cs
+++ fruit/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace fruit
+﻿using Newtonsoft.Json;
+using System.Reflection;
+
+namespace fruit
 {
     internal class Program
     {
+        class Fruit
+        {
+            string name;
+        }
+
+        static string Path = @".\Fruits.json";
         static void Main(string[] args)
         {
+
             
+        }
+
+        static void FruitJson()
+        {
+            // check if file exists
+            if (!File.Exists(Path)) { Console.WriteLine("File not found: " + Path); return; }
+
+            // read file
+            string jsonText = File.ReadAllText(Path);
+
+            // deserialise json to an array of fruit objects
+            Fruit[] fruits = JsonConvert.DeserializeObject<Fruit[]> (jsonText);
+
         }
     }
 }
