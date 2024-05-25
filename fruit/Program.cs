4f67bed64bb14f098e778aafb15c30c7c326ee4d
Hungermanw3t <mowsonben@gmail.com>
2024-05-26 00:04:07 +1000

    added the classes using VS's past special feature. Need to ask sean how i'm meant to do it because this surely isn't it.

--- fruit/Program.cs
+++ fruit/Program.cs
@@ -1,33 +1,50 @@
 ﻿using Newtonsoft.Json;
-using System.Reflection;
 
 namespace fruit
 {
     internal class Program
     {
-        class Fruit
+        static string Path = @".\Fruits.json";
+
+
+        public class Fruit
         {
-            string name;
+            public string? name { get; set; }
+            public int id { get; set; }
+            public string? family { get; set; }
+            public string? order { get; set; }
+            public string? genus { get; set; }
+            public Nutritions? nutritions { get; set; }
         }
 
-        static string Path = @".\Fruits.json";
+        public class Nutritions
+        {
+            public int calories { get; set; }
+            public float fat { get; set; }
+            public float sugar { get; set; }
+            public float carbohydrates { get; set; }
+            public float protein { get; set; }
+        }
+
+
         static void Main(string[] args)
         {
-
-            
+            // Create Fruit object (deserialised fruit json)
+            Fruit[]? DsFJ = FruitJson();
         }
 
-        static void FruitJson()
+        private static Fruit[]? FruitJson()
         {
             // check if file exists
-            if (!File.Exists(Path)) { Console.WriteLine("File not found: " + Path); return; }
+            if (!File.Exists(Path)) { Console.WriteLine("File not found: " + Path); throw new FileNotFoundException { }; }
 
             // read file
             string jsonText = File.ReadAllText(Path);
 
             // deserialise json to an array of fruit objects
-            Fruit[] fruits = JsonConvert.DeserializeObject<Fruit[]> (jsonText);
+            Fruit[]? fruits = JsonConvert.DeserializeObject<Fruit[]>(jsonText);
 
+            return fruits;
         }
     }
 }
